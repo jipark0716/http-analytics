99e2d79c245f37fa1f1fc9060fa6c4a2a93e1ec8
jipark0716 <jipark0716@gmail.com>
2025-07-29 10:14:23 +0900

    feat: 잼민이클라이언트

--- http-analyze/src/main.rs
+++ http-analyze/src/main.rs
@@ -1,14 +1,60 @@
-use gemini_client_api::gemini::ask::Gemini;
-use gemini_client_api::gemini::types::sessions::Session;
+use reqwest::Client;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Part {
+    text: String,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "lowercase")]
+enum Role {
+    User,
+}
+
+#[derive(Serialize)]
+struct Content {
+    role: Role,
+    parts: Vec<Part>,
+}
+
+#[derive(Serialize)]
+struct GenerateContentRequest {
+    system_instruction: Content,
+    contents: Vec<Content>,
+}
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let mut session = Session::new(10);
-    let api = Gemini::new(std::env::var("GEMINI_API_KEY")?, "gemini-1.5-flash", None);
+async fn main() {
+    // let api_key = std::env::var("GEMINI_API_KEY").expect("miss env: GEMINI_API_KEY");
+    let api_key = "ㅁㄴㅇ".to_string();
+    let model = "gemini-2.5-flash-lite".to_string();
 
-    let user_input = "최근 로그인한 사람들의 전화번호를 쿼리로 알려줘";
-    let response = api.ask(session.ask_string(user_input)).await?;
-    println!("LLM 응답:\n{}", response.get_text(""));
+    let req = GenerateContentRequest {
+        system_instruction: Content {
+            role: Role::User,
+            parts: vec![Part {
+                text: "안녕이라하면 벅벅이라고 해줘".to_string(),
+            }],
+        },
+        contents: vec![Content {
+            role: Role::User,
+            parts: vec![Part {
+                text: r#"안녕"#.to_string(),
+            }],
+        }],
+    };
 
-    Ok(())
+    let client = Client::new();
+    let res = client
+        .post(format!("https://generativelanguage.googleapis.com/v1beta/models/{model}:generateContent"))
+        .header("x-goog-api-key", api_key)
+        .header("Content-Type", "application/json")
+        .json(&req)
+        .send()
+        .await
+        .unwrap();
+
+    let text = res.text().await.unwrap();
+    println!("{}", text);
 }
